be4f12be85dbaf4dcba85bc61d94742f1ca11fdc
DanielLaszczych <daniellaszczych@gmail.com>
2022-01-28 21:53:54 -0500

    Temp name change

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,6 +1,6 @@
 const eventResolvers = require('./event-resolvers');
 const matchFormResolvers = require('./matchForm-resolvers');
-const pitFormResolvers = require('./tempName');
+const pitFormResolvers = require('./pitForm-resolvers');
 const usersResolvers = require('./users-resolvers');
 
 module.exports = [usersResolvers, pitFormResolvers, eventResolvers, matchFormResolvers];
