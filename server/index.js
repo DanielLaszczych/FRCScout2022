e1dcced2c877f750a98de10d2b20216cf93722c3
Daniel Laszczych <dlaszczych@BoyceTechnologies.com>
2022-03-15 09:29:16 -0400

    Force https

--- server/index.js
+++ server/index.js
@@ -31,6 +31,16 @@ const corsPolicy = async (req, res, next) => {
 app.options('*', cors());
 app.use(corsPolicy);
 
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+app.use(requireHTTPS);
+
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
