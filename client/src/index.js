2245b31077be25bee131d8d7dd6c286f77ddc19c
DanielLaszczych <daniellaszczych@gmail.com>
2022-03-21 21:50:00 -0400

    Removed service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import { unregister } from './registerServiceWorker';
 
 // import { ColorModeScript } from '@chakra-ui/react';
 // import theme from './theme';
@@ -46,8 +45,6 @@ const client = new ApolloClient({
     credentials: 'include',
     cache: cache,
 });
-
-unregister();
 
 ReactDOM.render(
     <React.StrictMode>
